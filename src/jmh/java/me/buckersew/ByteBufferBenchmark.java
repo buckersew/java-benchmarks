b006022f3b61ebd9164495a5218d048082aa5071
edbuckney <29359594+buckerslondon@users.noreply.github.com>
2020-04-22 10:50:24 +0100

    amending bytebuffer benchmark to properly work!

--- src/jmh/java/me/buckersew/ByteBufferBenchmark.java
+++ src/jmh/java/me/buckersew/ByteBufferBenchmark.java
@@ -1,54 +1,69 @@
 package me.buckersew;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.*;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 public class ByteBufferBenchmark {
-    ByteBuffer _nativeBufferLE;
-    ByteBuffer _heapBufferLE;
-    ByteBuffer _nativeBufferNativeOrder;
-    ByteBuffer _heapBufferNativeOrder;
+    private static final byte[] name = "TESTTEST".getBytes();
 
-    @Setup
-    public void createBuffers() {
-        _nativeBufferLE = ByteBuffer.allocateDirect(1024).order(ByteOrder.LITTLE_ENDIAN);
-        _heapBufferLE = ByteBuffer.allocate(1024).order(ByteOrder.LITTLE_ENDIAN);
-        _nativeBufferNativeOrder = ByteBuffer.allocateDirect(1024).order(ByteOrder.nativeOrder());
-        _heapBufferNativeOrder = ByteBuffer.allocate(1024).order(ByteOrder.nativeOrder());
+    @State(Scope.Benchmark) // shared across all threads - given only one thread irrelevant
+    public static class BufferState {
+        ByteBuffer nativeBufferLE;
+        ByteBuffer heapBufferLE;
+        ByteBuffer nativeBufferNativeOrder;
+        ByteBuffer heapBufferNativeOrder;
+
+        @Setup(Level.Trial) // once for each full run of the benchmark
+        public void createBuffers() {
+            nativeBufferLE = ByteBuffer.allocateDirect(1024).order(ByteOrder.LITTLE_ENDIAN);
+            heapBufferLE = ByteBuffer.allocate(1024).order(ByteOrder.LITTLE_ENDIAN);
+            nativeBufferNativeOrder = ByteBuffer.allocateDirect(1024).order(ByteOrder.nativeOrder());
+            heapBufferNativeOrder = ByteBuffer.allocate(1024).order(ByteOrder.nativeOrder());
+        }
+    }
+    @Benchmark
+    public ByteBuffer benchmarkNativeLE(BufferState state) {
+        state.nativeBufferLE.clear();
+        state.nativeBufferLE.putLong(345L);
+        state.nativeBufferLE.putInt(20);
+        state.nativeBufferLE.put(name);
+        state.nativeBufferLE.flip();
+        // by returning we are making sure the VM doesn't optimise this for dead code
+        return state.nativeBufferLE;
     }
 
     @Benchmark
-    public ByteBuffer benchmarkNativeLE() {
-        _nativeBufferLE.putLong(345L);
-        _nativeBufferLE.putInt(20);
+    public ByteBuffer benchmarkHeapLE(BufferState state) {
+        state.heapBufferLE.clear();
+        state.heapBufferLE.putLong(345L);
+        state.heapBufferLE.putInt(20);
+        state.heapBufferLE.put(name);
+        state.heapBufferLE.flip();
         // by returning we are making sure the VM doesn't optimise this for dead code
-        return _nativeBufferLE;
+        return state.heapBufferLE;
     }
 
     @Benchmark
-    public ByteBuffer benchmarkHeapLE() {
-        _heapBufferLE.putLong(345L);
-        _heapBufferLE.putInt(20);
+    public ByteBuffer benchmarkNativeNativeOrder(BufferState state) {
+        state.nativeBufferNativeOrder.clear();
+        state.nativeBufferNativeOrder.putLong(345L);
+        state.nativeBufferNativeOrder.putInt(20);
+        state.nativeBufferNativeOrder.put(name);
+        state.nativeBufferNativeOrder.flip();
         // by returning we are making sure the VM doesn't optimise this for dead code
-        return _heapBufferLE;
+        return state.nativeBufferNativeOrder;
     }
 
     @Benchmark
-    public ByteBuffer benchmarkNativeNativeOrder() {
-        _nativeBufferNativeOrder.putLong(345L);
-        _nativeBufferNativeOrder.putInt(20);
+    public ByteBuffer benchmarkHeapNativeOrder(BufferState state) {
+        state.heapBufferNativeOrder.clear();
+        state.heapBufferNativeOrder.putLong(345L);
+        state.heapBufferNativeOrder.putInt(20);
+        state.heapBufferNativeOrder.put(name);
+        state.heapBufferNativeOrder.flip();
         // by returning we are making sure the VM doesn't optimise this for dead code
-        return _nativeBufferNativeOrder;
-    }
-
-    @Benchmark
-    public ByteBuffer benchmarkHeapNativeOrder() {
-        _heapBufferNativeOrder.putLong(345L);
-        _heapBufferNativeOrder.putInt(20);
-        // by returning we are making sure the VM doesn't optimise this for dead code
-        return _heapBufferNativeOrder;
+        return state.heapBufferNativeOrder;
     }
 }
